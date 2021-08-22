3e938c37f9f889bcb8df25e6c99c3697dc80c746
Tristan Kennedy <tristan@taskinoz.com>
2021-08-22 21:39:18 +1000

    Setup build script for automated deployments

--- build.sh
+++ build.sh
@@ -5,8 +5,24 @@
 ## Add the VPK that your editing in a /dir folder
 ## eg. englishclient_frontend.bsp.pak000_dir.vpk
 
-cp ./dir/englishclient_frontend.bsp.pak000_dir.vpk ./ & cp -R ./src ./englishclient_frontend.bsp.pak000_dir
-wait
-RSPNVPK englishclient_frontend.bsp.pak000_dir.vpk -s
-wait
-rm -rf ./englishclient_frontend.bsp.pak000_dir
+if [ "$1" = "prod" ]
+then
+  ## Run on Prod
+  cp -R ./src ./englishclient_mp_common.bsp.pak000_dir
+  wait
+  ./RSPNVPK englishclient_mp_common.bsp.pak000_dir.vpk -s
+  wait
+  rm -rf ./englishclient_mp_common.bsp.pak000_dir
+  mkdir "Enhanced Menu"
+  mv ./*.vpk ./"Enhanced Menu"/
+  zip -r "Enhanced Menu Compiled VPK.zip" ./"Enhanced Menu"/
+  rm -rf ./"Enhanced Menu"/
+else
+  ## Run on Dev
+  cp ./dir/englishclient_frontend.bsp.pak000_dir.vpk ./ & cp -R ./src ./englishclient_frontend.bsp.pak000_dir
+  wait
+  RSPNVPK englishclient_frontend.bsp.pak000_dir.vpk -s
+  wait
+  rm -rf ./englishclient_frontend.bsp.pak000_dir
+  mv ./*.vpk /d/Games/Titanfall2/vpk/
+fi
