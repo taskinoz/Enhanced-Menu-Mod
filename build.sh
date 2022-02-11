220c4d52b35c8e8024e12c2812826800dfe89f94
Tristan Kennedy <tristan@taskinoz.com>
2022-02-11 20:56:37 +1100

    Added Northstar and Icepick build options

--- build.sh
+++ build.sh
@@ -24,5 +24,24 @@ else
   RSPNVPK englishclient_frontend.bsp.pak000_dir.vpk -s
   wait
   rm -rf ./englishclient_frontend.bsp.pak000_dir
-  mv ./*.vpk /d/Games/Titanfall2/vpk/
+  # mv ./*.vpk "C:\Program Files (x86)\Origin Games\Titanfall2\vpk"
+fi
+
+## Northstar and Icepick Changes
+mkdir ./Icepick
+cp -R ./src/* ./Icepick && cp -R ./ice/* ./Icepick
+rm ./Icepick/scripts/vscripts/scripts.rson
+mkdir ./Northstar
+cp -R ./src/ ./Northstar/mod && cp -R ./nstar/* ./Northstar
+rm ./Northstar/mod/scripts/vscripts/scripts.rson
+
+mv ./Icepick ./Enhanced.Menu.Mod.Icepick
+mv ./Northstar ./Enhanced.Menu.Mod.Northstar
+
+if [ "$1" = "prod" ]
+then
+  zip -r "Enhanced.Menu.Mod.Icepick.zip" ./"Enhanced.Menu.Mod.Icepick"/
+  rm -rf ./"Enhanced.Menu.Mod.Icepick"/
+  zip -r "Enhanced.Menu.Mod.Northstar.zip" ./"Enhanced.Menu.Mod.Northstar"/
+  rm -rf ./"Enhanced.Menu.Mod.Northstar"/
 fi
